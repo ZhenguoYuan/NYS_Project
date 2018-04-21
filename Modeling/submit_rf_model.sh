dbbab233f32527a2560b02af9c411439be65bf50
jbi6 <jbi6@hpc2.sph.emory.edu>
2018-04-20 23:16:52 -0400

    RF modeling submit to short.q

--- Modeling/submit_rf_model.sh
+++ Modeling/submit_rf_model.sh
@@ -3,6 +3,6 @@
 for((year=2015;year<=2015;year++))
 do
      name=RFModel$year
-     qsub -N $name -q long.q -o /home/jbi6/log -e /home/jbi6/log qsub_script_rf_model $year
+     qsub -N $name -q short.q -o /home/jbi6/log -e /home/jbi6/log qsub_script_rf_model $year
 done
 
