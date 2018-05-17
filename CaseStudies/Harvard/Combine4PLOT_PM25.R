1c711476c7f2c2af92f93f78c810321dbe4341cf
Bijianzhao <bjz91@qq.com>
2018-05-17 19:20:27 -0400

    modify combine harvard

--- CaseStudies/Harvard/Combine4PLOT_PM25.R
+++ CaseStudies/Harvard/Combine4PLOT_PM25.R
@@ -20,6 +20,7 @@ numdays <- numOfYear(as.numeric(year))
 combinePlot <- function(year, numdays, inpath, outpath) {
   ## ---------- Combining ---------- ##
   pm25_combine_plot <- combine4pm25RData(c(1 : numdays), year, inpath) # All year
+  pm25_combine_plot_snow <- combine4pm25RData(c(1 : 105), year, inpath) # Snow Season
   pm25_combine_plot_spring <- combine4pm25RData(c(61 : 152), year, inpath) # Spring
   pm25_combine_plot_summer <- combine4pm25RData(c(153 : 244), year, inpath) # Summer
   pm25_combine_plot_fall <- combine4pm25RData(c(245 : 336), year, inpath) # Fall
@@ -27,6 +28,7 @@ combinePlot <- function(year, numdays, inpath, outpath) {
   
   ## ---------- Output ---------- ##
   save(pm25_combine_plot, file = file.path(outpath, 'pm25_combine_plot.RData'))
+  save(pm25_combine_plot_snow, file = file.path(outpath, 'pm25_combine_plot_snow.RData'))
   save(pm25_combine_plot_spring, file = file.path(outpath, 'pm25_combine_plot_spring.RData'))
   save(pm25_combine_plot_summer, file = file.path(outpath, 'pm25_combine_plot_summer.RData'))
   save(pm25_combine_plot_fall, file = file.path(outpath, 'pm25_combine_plot_fall.RData'))
