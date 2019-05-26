825466e00d8938fa34c14121930c3274c29ab723
Bijianzhao <bjz91@qq.com>
2019-05-26 18:36:16 +0800

    update ndvi

--- NDVI/ndvi.R
+++ NDVI/ndvi.R
@@ -14,60 +14,62 @@ setwd('/home/jbi6/NYS_Project/NDVI/')
 source('../src/latlon.R')
 source('../src/fun.R')
 
-# Arguments from R script
-Args <- commandArgs()
-# Time 
-year <- as.numeric(Args[6])
-ndays <- numOfYear(year)
-
-# MAIAC grid
-new.loc <- read.csv('../MAIAC_GRID/output/maiac_grid.csv', stringsAsFactors = F)
-# Paths
-in.path <- file.path('/home/jbi6/envi/NYS_Output/NDVI_ORI', as.character(year))
-out.path <- file.path('/home/jbi6/envi/NYS_Output/MAIAC_GRID_OUTPUT/NDVI', as.character(year))
-if (!file.exists(out.path)) {
-  dir.create(out.path, recursive = T)
-}
-
-# Initialization
-ndvi.name.pre <- ''
-ndvi.sub.new <- NULL
-
-# For each day
-for (i in 1 : ndays) {
+for (year in 2002 : 2012) {
   
-  # Determine the file name
-  month <- month(as.Date(i - 1, origin = paste(as.character(year), '-01-01', sep = ''))) # Corresponding month of the day
-  first.day <- yday(as.Date(paste(as.character(year), sprintf('%02d', month), '01', sep = '-'))) # The first day of that month
-  ndvi.name <- sprintf('NewYork_1km_monthly_VI_%4d_%03d.csv', year, first.day)
+  ndays <- numOfYear(year)
   
-  if (ndvi.name != ndvi.name.pre) {
+  # MAIAC grid
+  new.loc <- read.csv('../MAIAC_GRID/output/maiac_grid.csv', stringsAsFactors = F)
+  # Paths
+  in.path <- file.path('/home/jbi6/envi/NYS_Output/NDVI_ORI', as.character(year))
+  out.path <- file.path('/home/jbi6/envi/NYS_Output/MAIAC_GRID_OUTPUT/NDVI', as.character(year))
+  if (!file.exists(out.path)) {
+    dir.create(out.path, recursive = T)
+  }
+  
+  # Initialization
+  ndvi.name.pre <- ''
+  ndvi.sub.new <- NULL
+  
+  # For each day
+  for (i in 1 : ndays) {
     
-    # Read the NDVI file
-    ndvi <- read.csv(file = file.path(in.path, ndvi.name), stringsAsFactors = F, as.is = T)
-    ndvi$X_Lon <- as.numeric(ndvi$X_Lon)
-    # Subset the NDVI
-    ndvi.sub <- subset(ndvi, Y_Lat >= min(lat.range) & Y_Lat <= max(lat.range) & X_Lon >= min(lon.range) & X_Lon <= max(lon.range))
+    # Determine the file name
+    month <- month(as.Date(i - 1, origin = paste(as.character(year), '-01-01', sep = ''))) # Corresponding month of the day
+    first.day <- yday(as.Date(paste(as.character(year), sprintf('%02d', month), '01', sep = '-'))) # The first day of that month
+    ndvi.name <- sprintf('NewYork_1km_monthly_VI_%4d_%03d.csv', year, first.day)
     
-    #IDW
-    ndvi.sub.new <- idw.interp(ndvi.sub$X_Lon, ndvi.sub$Y_Lat, ndvi.sub$NDVI,
-                               new.loc$Lon, new.loc$Lat, nmax = 3) # For Cloud Fraction 
-    names(ndvi.sub.new)[1] <- 'lon'
-    names(ndvi.sub.new)[2] <- 'lat'
-    names(ndvi.sub.new)[3] <- 'NDVI'
+    if (ndvi.name != ndvi.name.pre) {
+      
+      # Read the NDVI file
+      ndvi <- read.csv(file = file.path(in.path, ndvi.name), stringsAsFactors = F, as.is = T)
+      ndvi$X_Lon <- as.numeric(ndvi$X_Lon)
+      # Subset the NDVI
+      ndvi.sub <- subset(ndvi, Y_Lat >= min(lat.range) & Y_Lat <= max(lat.range) & X_Lon >= min(lon.range) & X_Lon <= max(lon.range))
+      
+      #IDW
+      ndvi.sub.new <- idw.interp(ndvi.sub$X_Lon, ndvi.sub$Y_Lat, ndvi.sub$NDVI,
+                                 new.loc$Lon, new.loc$Lat, nmax = 3) # For Cloud Fraction 
+      names(ndvi.sub.new)[1] <- 'lon'
+      names(ndvi.sub.new)[2] <- 'lat'
+      names(ndvi.sub.new)[3] <- 'NDVI'
+      
+    }
+    
+    # Save file
+    out.file.name <- sprintf('%d%03d_NDVI.RData', year, i)
+    save(ndvi.sub.new, file = file.path(out.path, out.file.name))
+    
+    # Update ndvi input file name
+    ndvi.name.pre <- ndvi.name
+    
+    print(out.file.name)
     
   }
   
-  # Save file
-  out.file.name <- sprintf('%d%03d_NDVI.RData', year, i)
-  save(ndvi.sub.new, file = file.path(out.path, out.file.name))
-  
-  # Update ndvi input file name
-  ndvi.name.pre <- ndvi.name
-  
-  print(out.file.name)
-
 }
+
+
 
 
 
